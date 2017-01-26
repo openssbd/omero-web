cec24b17d781d65fc9f120263413cf2b62b05b4c
Yukako Tohsato <yukako.tohsato@riken.jp>
2017-01-26 18:23:10 +0900

    add run script

---
+++ run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## ENV PG_DATA_FLG 1にしたDockerfileを使ってイメージを作成する
+docker build -t omero-web:1 ~/omero-web/.
+
+## 旧コンポーネントの削除
+docker stop omero-web
+docker rm omero-web
+
+## 作成したイメージから新しくコンポーネントの作成しログイン (開発用)
+## ログイン後 root で /start.sh を実行するとサービスが起動する
+docker run -it -p 80:80 -p 4064:4064 --name omero-web omero-web:1 /bin/bash
+
+## 作成したイメージから新しくコンポーネントの作成しバックグラウンドモードで起動 (サービス用)
+#docker run  -d -p 80:80 -p 4064:4064 --name omero-web omero-web:1
